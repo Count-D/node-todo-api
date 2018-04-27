399786349dc56e35f5f96b225a110ba0b174c868
Dejan Dabic <dejan.dabic023@gmail.com>
2018-04-27 14:22:49 +0200

    Add GET /users/me and authenticate

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -95,15 +96,19 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
-    
+
     user.save().then(() => {
-        user.generateAuthToken();
+       return user.generateAuthToken();
     }).then((token) => {
         res.header('x-auth', token).send(user)
     }).catch((e) => {
         res.status(400).send(e);
     })
-})
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
