77825b297ec7ab0dd84f1f303bc1c892bd2e8828
Dejan Dabic <dejan.dabic023@gmail.com>
2018-04-28 11:40:42 +0200

    Hashed passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+var hashedPass = '$2a$10$02yMRvyV/fpjA9bdfKvyc.iewFKJBWEy2vWVyfItoO2v8ssPLtVzq';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
+
+
+
+
 
 // var message = 'I am user Nr 3';
 
